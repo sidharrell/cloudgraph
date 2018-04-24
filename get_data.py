e9b2c4f3de425153db476117a4c75c0c00d7460f
root <root@ip-172-31-60-106.us-gov-west-1.compute.internal>
2018-04-24 01:31:10 +0000

    changes

---
+++ get_data.py
@@ -0,0 +1,43 @@
+import datetime
+from dateutil.tz import tzlocal
+from pprint import pprint
+import boto3
+import json
+import os
+
+with open('instances.json') as json_data:
+  d = json.load(json_data)
+with open('times.json') as json_data:
+  t = json.load(json_data)
+with open('metrics.json') as json_data:
+  m = json.load(json_data)
+for instance in d['myinstances']:
+  for time in t['mytimes']:
+    if not os.path.exists(time['start']):
+      os.makedirs(time['start'])
+    for metric in m['mymetrics']:
+      f = open(time['start']+'/'+instance['name']+'-'+metric['metricName'], 'w')
+      f.write("Date,"+metric['title']+"\n")
+      client = boto3.client('cloudwatch')
+      responses = client.get_metric_statistics(
+        Namespace='AWS/EC2',
+        MetricName=metric['metricName'],
+        Dimensions=[
+          {
+            'Name': 'InstanceId',
+            'Value': instance['id']
+          },
+        ],
+        StartTime=time['start'],
+        EndTime=time['end'],
+        Period=3600,
+        Statistics=[metric['statisticValues']]
+      )
+#pprint(responses['Datapoints'])
+      mylist=[]
+      datapoints=responses['Datapoints']
+      def getKey(custom):
+        return custom['Timestamp'].timestamp()
+      datapoints.sort(key=getKey)
+      for data in datapoints:
+        f.write("%s,%s\n" % (data['Timestamp'].timestamp(),data[metric['statisticValues']]))
